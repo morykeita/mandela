0b633e53f00c728ad1f5588d62b99299739938d9
Mory Keita <morykeita1994@gmail.com>
2017-12-18 07:31:56 -0500

    project skeleton

---
+++ src/main/java/com/morykeita/mandela/MandelaApplication.java
@@ -0,0 +1,12 @@
+package com.morykeita.mandela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandelaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/morykeita/mandela/MandelaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.morykeita.mandela;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MandelaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
